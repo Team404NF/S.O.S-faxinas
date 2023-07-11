ebaccc16a47b173d2073d097ea0005c359395eff
JG-Stadler <joaogbstadler@gmail.com>
2023-07-11 13:54:57 -0300

    alpha1.0
    melhorei a animação do menu, adicionei um botão dropdown e adicionei conteúdo de apresentação.

---
+++ js/button.js
@@ -0,0 +1,8 @@
+function ExibirConteudo(){
+    var a = document.getElementById('contratar');
+    if(a.style.display === "block"){
+        a.style.display = "none"
+    }else{
+        a.style.display = "block"
+    }
+}
