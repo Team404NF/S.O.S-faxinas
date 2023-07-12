9814d31069214146cfb5dcbe580411193f89fda0
JG-Stadler <joaogbstadler@gmail.com>
2023-07-12 10:44:58 -0300

    alpha 1.3
    melhorei o botão, alinhei o main, e reconfigurei o menu

--- js/button.js
+++
@@ -1,8 +0,0 @@
-function ExibirConteudo(){
-    var a = document.getElementById('contratar');
-    if(a.style.display === "block"){
-        a.style.display = "none"
-    }else{
-        a.style.display = "block"
-    }
-}
--- js/menu.js
+++ js/menu.js
@@ -3,4 +3,6 @@ function actMenu(){
     nav.classList.toggle("nav-act");
     var menu = document.querySelector(".hamburger");
     menu.classList.toggle("menu-act");
+    var body = document.getElementById("body");
+    body.classList.toggle("body-act");
 }
