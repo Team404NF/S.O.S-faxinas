4b4e49d76a04a9161f4e005fc54361cd67a75ea1
JG-Stadler <joaogbstadler@gmail.com>
2023-07-31 13:45:26 -0300

    versão 1.2
    link do whats consertado, headers personalizados e arquivos css diminuidos

--- js/button.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector(".contact");
-const contacts = document.querySelector(".contacts");
-
-button.addEventListener("click", (event)=>{
-   contacts.classList.toggle("contactsAct");
-});
